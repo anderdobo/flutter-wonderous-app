cce3eb36bec44497c1712b680a67fe37aa004312
Eric Windmill <ewindmill@gmail.com>
2023-08-11 13:02:47 -0700

    Update

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/shawn/Dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/shawn/Dev/gskinner/flutter-wonders-app"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\amdob\StudioProjects\flutter-wonderous-app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=2.0.3"
-export "FLUTTER_BUILD_NUMBER=2.0.3"
+export "FLUTTER_BUILD_NAME=2.0.6"
+export "FLUTTER_BUILD_NUMBER=2.0.6"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
